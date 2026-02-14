e53823b7a85adbcfb56a79e5f812288186a3a2e9
robin <robbloomstreet@gmail.com>
2026-02-14 21:52:31 +0600

    feat: tcp echo server

--- src/main.c
+++ src/main.c
@@ -1,6 +1,74 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int tcpListen(int port) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    int opt = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        return -1;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        return -1;
+    }
+
+    if (listen(fd, 10)) {
+        perror("listen");
+        return -1;
+    }
+
+    return fd;
+}
 
 int main(void) {
-    printf("Hello, world\n");
+    int fd = tcpListen(8000);
+    if (fd == -1) {
+        return -1;
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t len = sizeof(client_addr);
+    char buf[1024];
+
+    while (1) {
+        int client_fd = accept(fd, (struct sockaddr *)&client_addr, &len);
+        if (client_fd == -1) {
+            perror("accept");
+            return -1;
+        }
+
+        printf("Connected to ==> %s:%d\n",
+               inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port));
+
+        while (1) {
+            ssize_t bytes_read = read(client_fd, &buf, 1024 - 1);
+            if (bytes_read <= 0) {
+                break;
+            }
+            buf[bytes_read] = '\0';
+            write(client_fd, buf, bytes_read);
+        }
+
+        close(client_fd);
+    }
+
+    close(fd);
+
     return 0;
 }
---
+++ utils/str.h
@@ -0,0 +1,95 @@
+/*
+* SDS style string library
+*/
+
+#ifndef STR_H
+#define STR_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef char *String;
+
+/// constructors and destructors
+String strNew(const char *s);
+String strNewLen(const void *s, const uint32_t len);
+String strEmpty(void);
+void strFree(String s);
+
+#ifdef STRING_IMPLEMENTATION
+
+// header for String type
+// used as a binary perfix that's stored before the actual String type
+typedef struct {
+    uint32_t len_;
+    uint32_t alloc_;
+} StrHdr_;
+
+// get pointer to header from String
+static inline StrHdr_ *getStrHdr_(const String s) {
+    return ((StrHdr_ *)s - 1);
+}
+
+// get available memory space from String
+static inline uint32_t getStrAvail_(const String s) {
+    return (getStrHdr_(s)->alloc_ - getStrHdr_(s)->len_);
+}
+
+static inline uint32_t getStrLen_(const String s) {
+    return (getStrHdr_(s)->len_);
+}
+
+static inline uint32_t getStrAlloc_(const String s) {
+    return (getStrHdr_(s)->alloc_);
+}
+
+String strNewLen(const void *s, const uint32_t len) {
+    StrHdr_ *hdr = (StrHdr_ *)malloc(sizeof(StrHdr_) + len + 1);
+    if (!hdr) {
+        return NULL;
+    }
+
+    hdr->len_ = len;
+    hdr->alloc_ = len;
+
+    String str = (String)(hdr + 1);
+    if (s && len > 0) {
+        memcpy(str, s, len);
+    }
+    str[len] = '\0';
+
+    return str;
+}
+
+String strEmpty(void) {
+    return strNewLen("", 0);
+}
+
+String strNew(const char *s) {
+    if (!s) {
+        return NULL;
+    }
+
+    return strNewLen(s, strlen(s));
+}
+
+void strFree(String s) {
+    if (!s) {
+        return;
+    }
+
+    StrHdr_ *hdr = getStrHdr_(s);
+    free(hdr);
+}
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
